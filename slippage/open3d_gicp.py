ADD wrong computation just for try
Signed-off-by: WeisonWeileen <weisonweileen@gmail.com>

slippage/open3d_gicp.py
@@ -10,6 +10,9 @@ folder = "./pointcloud_raw/"
 T_overall_fl = np.eye(4)
 T_overall_fr = np.eye(4)
 
+pc_frame_num = 55
+
+
 
 def pc_processing(pc):
     points = np.asarray(pc.points)
@@ -21,7 +24,7 @@ def pc_processing(pc):
     return pc
 
 if __name__ == "__main__":
-    for i in range(55):
+    for i in range(pc_frame_num):
     # for i in range(1):
         # 读取源点云和目标点云
         source = o3d.io.read_point_cloud(f"{folder}{i}.ply")
@@ -38,49 +41,56 @@ if __name__ == "__main__":
                                 [0.0, 1.0, 0.0, 0.0],
                                 [0.0, 0.0, 1.0, 0.0],
                                 [0.0, 0.0, 0.0, 1.0]])  # 设置初始的变换矩阵
+        # 一帧粗匹配，然后一帧精匹配
 
-        # 运行GICP算法
-        reg_p2p = o3d.pipelines.registration.registration_generalized_icp(
+        # 运行GICP算法, 对应粗匹配
+        reg_p2p_gen = o3d.pipelines.registration.registration_generalized_icp(
             source, target, max_correspondence_distance=0.05, init=trans_init,
             estimation_method=o3d.pipelines.registration.TransformationEstimationForGeneralizedICP())  
+        
+        # 运行ICP算法，对应精匹配
+        threshold = 0.01
+        reg_p2p =  o3d.pipelines.registration.registration_icp(
+            source.transform(reg_p2p_gen.transformation),
+            target,
+            threshold,
+            trans_init,
+            o3d.pipelines.registration.TransformationEstimationPointToPoint())        
+        # source_transformed = source_transformed.transform(reg_p2p.transformation)
 
-        # 将变换后的点云应用于源点云
-        source_transformed = source_transformed.transform(reg_p2p.transformation)
-
-        T = reg_p2p.transformation
-
-        # 打印变换矩阵
-        # print("变换矩阵:\n", T)
-
-
-        # print(T)
-        T_overall_fl = np.dot(T, T_overall_fl)
-        T_overall_fr = np.dot(T_overall_fr, T)
-
-    print("变换矩阵:\n", T_overall_fl)
-    print("变换矩阵:\n", T_overall_fr)
+        #累乘，将两帧间最终得到的点云的矩阵进行点乘
+        T_temp_fl = reg_p2p.transformation
+        T_overall_fl = np.dot(T_temp_fl, T_overall_fl)
 
 
     ##### ICP start
 
-    threshold = 0.01  # 设置一个阈值，用于剔除大于该阈值的配准误差点
+    # threshold = 0.01  # 设置一个阈值，用于剔除大于该阈值的配准误差点
     
-    # 运行ICP算法
-    reg_p2p = o3d.pipelines.registration.registration_icp(
-        origin_pc, target, threshold, T_overall_fl,
-        o3d.pipelines.registration.TransformationEstimationPointToPoint())
+    # # 运行ICP算法
+    # reg_p2p = o3d.pipelines.registration.registration_icp(
+    #     origin_pc, target, threshold, T_overall_fl,
+    #     o3d.pipelines.registration.TransformationEstimationPointToPoint())
+# origin_pc
+    
 
-    # 将变换后的点云应用于源点云
-    source_transformed = origin_pc.transform(reg_p2p.transformation)
+    # T = reg_p2p.transformation
 
-    T = reg_p2p.transformation
-
-    print("变换矩阵 after ICP:\n", T)
+    # print("变换矩阵 after ICP:\n", T)
 
     #### ICP end
 
+    # 将变换后的点云应用于源点云
+        
+
+    source_transformed = origin_pc.transform(T_overall_fl)
 
     # 可视化结果
+    target = o3d.io.read_point_cloud(f"{folder}{pc_frame_num-1}.ply")
+
+    print("变换矩阵 after ICP:\n", T_overall_fl)
+
+
     source_transformed.paint_uniform_color([0, 1, 0])
     target.paint_uniform_color([0, 0, 1])
     o3d.visualization.draw_geometries([source_transformed, target])